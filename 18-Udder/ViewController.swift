507603e194062cd6e8b75d80ac20ca63e4810e20
Bill Plantz <wplantz@gmail.com>
2020-07-10 20:12:59 -0400

    Initial Commit

---
+++ 18-Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  18-Udder
+//
+//  Created by William Plantz on 7/10/20.
+//  Copyright © 2020 Bill Plantz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
